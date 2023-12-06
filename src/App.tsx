812fdcaec782a0a18e25f78453455ff1eb79c22f
Alwan Wirawan <alwanwrwn@student.uns.ac.id>
2023-12-06 21:05:59 +0700

    task management page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,54 @@
-import Alert from "./components/Alert";
-import ListGroup from "./components/ListGroup";
+import Tab from "./components/Tab";
 
 function App() {
-  const list = ["Alwan", "Wirawan", "Habib", "John"];
+  // const list = ["Alwan", "Wirawan", "Habib", "John"];
+  //
+  // const handleSelectItem = (item: string) => {
+  //   console.log(item);
+  // };
+  //
+  //
 
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
+  const status_list = ["in progress", "to do", "done", "in review"];
 
   return (
-    <div>
+    <>
+      <div className="container py-2">
+        <div className="row">
+          <h1 className="col-11">
+            <b>Task Management</b>
+          </h1>
+          <button
+            type="button"
+            className="btn btn-primary col-1"
+            data-bs-toggle="modal"
+            data-bs-target="#newTaskModal"
+            id="addButton"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="32"
+              height="32"
+              fill="white"
+              className="bi bi-plus-lg"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"
+              />
+            </svg>
+          </button>
+        </div>
+
+        <div className="row py-2 overflow-hidden">
+          {status_list.map((stat) => (
+            <Tab status={stat} key={stat} />
+          ))}
+        </div>
+      </div>
       {/* <ListGroup items={list} heading="Tes" onSelectItem={handleSelectItem} /> */}
-      <Alert>Tes nih</Alert>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/Tab.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import Card from "./Card";
+
+interface Props {
+  status: string;
+}
+
+function Tab({ status }: Props) {
+  const BASE_URL = `http://127.0.0.1:8000/api/get?status=${status}`;
+
+  const [cards, setCards] = useState([]);
+
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(BASE_URL)
+        .then((Response) => Response.json())
+        .then((data) => {
+          setCards(data);
+        });
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="col card bg-secondary py-2 border-0 mx-2" key={status}>
+      <h2 className="text-white">{status}</h2>
+      <div className="card-body overflow-scroll p-0">
+        {cards.map((card) => (
+          <Card
+            id={card.id}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Tab;
