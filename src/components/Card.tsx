c878fd2c97c3f68fb809f9f4be93c70267aefa7f
Alwan Wirawan <alwanwrwn@student.uns.ac.id>
2023-12-15 21:30:35 +0700

    create update function

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,6 +11,12 @@ function Card({ id, title, description }: Props) {
     });
   };
 
+  const UpdateCard = () => {
+    fetch(`http://127.0.0.1:8000`, {
+      method: "PUT",
+    });
+  };
+
   return (
     <>
       <div className="card mt-1">
